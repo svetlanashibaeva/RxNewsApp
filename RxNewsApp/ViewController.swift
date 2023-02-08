b9fecbef9981145a5034ec86e280df3e115a9639
Shibaeva Svetlana <68277146+svetlanashibaeva@users.noreply.github.com>
2023-02-08 15:15:09 +0500

    Initial Commit

---
+++ RxNewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxNewsApp
+//
+//  Created by Света Шибаева on 08.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
